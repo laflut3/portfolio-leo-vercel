87fd85fe3b17d19fe718e18ed2fba2deaec232ce
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-09-10 21:48:21 +0200

    feat(): utilisation du localstorage pour ne déclencher l'animation que toute les 12h

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-"use client";
+"use client"
 
-import Image from "next/image";
 import React, { useState, useEffect } from "react";
-import { getActualSituation } from "@/db/queries/select";
-
 import FifthSection from "@/components/home/FifthSection";
 import Preloader from "@/components/home/Preload";
 import CompanySection from "@/components/home/CompanySection";
@@ -11,25 +8,34 @@ import SkillSection from "@/components/home/SkillSection";
 import ThirdSection from "@/components/home/ThirdSection";
 import PresentationSection from "@/components/home/PresentationSection";
 
+const TWELVE_HOURS = 12 * 60 * 60 * 1000; // 12 heures en millisecondes
+
 export default function HomePage() {
-  const [preloaderVisible, setPreloaderVisible] = useState(true);
+  const [preloaderVisible, setPreloaderVisible] = useState(false);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setPreloaderVisible(false);
-    }, 2000);
+    const lastShown = localStorage.getItem("lastPreloaderShown");
+    const now = new Date().getTime();
 
-    return () => clearTimeout(timer);
+    if (!lastShown || now - parseInt(lastShown) > TWELVE_HOURS) {
+      setPreloaderVisible(true);
+      const timer = setTimeout(() => {
+        setPreloaderVisible(false);
+        localStorage.setItem("lastPreloaderShown", now.toString());
+      }, 2000);
+
+      return () => clearTimeout(timer);
+    }
   }, []);
 
   return (
-    <main className="w-full">
-      {preloaderVisible && <Preloader />}
-      <PresentationSection/>
-      <SkillSection/>
-      <ThirdSection/>
-      <CompanySection/>
-      <FifthSection/>
-    </main>
+      <main className="w-full">
+        {preloaderVisible && <Preloader />}
+        <PresentationSection />
+        <SkillSection />
+        <ThirdSection />
+        <CompanySection />
+        <FifthSection />
+      </main>
   );
 }
