69c30fb940565f6018692d7f0feb68250e620326
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-09-11 13:38:12 +0200

    feat(): flash message

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,33 @@
 import ClientNavbar from "@/components/navbar/ClientNavbar";
 import Footer from "@/components/Footer";
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import "../styles/global.css";
 import {Provider} from "@/app/provider";
+import FlashMessage from "@/components/utils/FlashMessage";
 
 export const metadata: Metadata = {
-  title: "LEO TORRES",
-  description: "Bienvenue sur mon portfolio",
+    title: "LEO TORRES",
+    description: "Bienvenue sur mon portfolio",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="fr">
-      <head>
-        <link rel="icon" href="/icon.png" />
-      </head>
-      <body className="font-akaya" suppressHydrationWarning={true}>
-      <Provider>
-        <ClientNavbar />
-        {children}
-        <Footer />
-      </Provider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="fr">
+        <head>
+            <link rel="icon" href="/icon.png"/>
+        </head>
+        <body className="font-akaya" suppressHydrationWarning={true}>
+        <Provider>
+            <ClientNavbar/>
+            {children}
+            <FlashMessage/>
+            <Footer/>
+        </Provider>
+        </body>
+        </html>
+    );
 }
