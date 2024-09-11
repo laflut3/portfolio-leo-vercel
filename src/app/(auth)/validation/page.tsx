c425da6db984a53f0e0129b4b50c49fac157ced9
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-09-11 14:17:50 +0200

    feat(): modif route

--- src/app/(auth)/validation/page.tsx
+++ src/app/(auth)/validation/page.tsx
@@ -1,6 +1,6 @@
 import ValidationSection from "@/components/auth/validation/ValidationSection";
 
-export default function AuthPage() {
+export default function ValidationPage() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between">
             <ValidationSection/>
