a9b28d49daf56d727f0a75c06c5c6ee570ac05e1
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-09-11 09:13:56 +0200

    feat(): ajout sharp

---
+++ src/app/(auth)/admin/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+
+const ProfilePage: React.FC = () => {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between">
+
+        </main>
+    );
+};
+
+export default ProfilePage;
