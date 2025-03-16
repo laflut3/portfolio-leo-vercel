84d4480fc39e7bba9f7c82457ed8d467ff37980c
jean-mulet <leo0609leo@gmail.com>
2025-03-16 13:32:46 +0100

    feat(): update page about

--- src/components/about/AboutMore.tsx
+++ src/components/about/AboutMore.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useState} from "react";
 
 export default function AboutMore() {
     const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
@@ -73,6 +73,26 @@ export default function AboutMore() {
         Actuellement en 3ÉME année de Bachelor Universitaire de Technologie (BUT) en informatique, mon choix de parcours s'est porté sur DACS (Déploiement d'Applications communicantes et sécurisées). Ce dernier me permet d’acquérir les connaissances nécessaires afin de travailler dans le domaine de la cybersécurité.
     `;
 
+    const poursuite = `
+        Actuellement étudiant en dernière année de BUT informatique parcours Déploiement d’Applications Communicantes et Sécurisées (DACS), j’ai effectué cette dernière année en alternance chez Crédit Agricole Technologies et Services. Cette expérience professionnelle en tant que développeur back-end Java Spring Boot m’a permis d’identifier mon objectif professionnel. Ma priorité absolue est de maintenir le lien avec le milieu professionnel, ce qui m'a poussé à cibler exclusivement des formations en alternance.
+
+        À plus long terme, mon ambition est de devenir ingénieur DevOps, DevSecOps ou expert en cybersécurité afin d’acquérir une expertise complète dans la chaîne de développement et de sécurisation des applications. Les postes que je vise sont expert technique, leader d’équipe ou encore manager. À terme, un de mes objectifs serait de créer ma propre entreprise spécialisée dans l’accompagnement des organisations dans leur transformation DevOps et la sécurisation de leurs systèmes, réseau et personnel. Ce projet me motive à développer en continu de nouvelles compétences techniques, tout en cultivant un sens stratégique et une vision globale du marché.
+
+        Afin d’en apprendre un peu plus sur les formations possibles dans cette voie, je me suis rendu au salon de l’étudiant à Montpellier où j’ai pu échanger avec un élève de l’INSA de Lyon (voir Annexe 1 : interview salon étudiant). Ces entretiens ont pu m'aider à choisir et cibler les écoles dans lesquelles j’aimerais aller, et donc dans mon choix d’intégrer une école d’ingénieur, tout en envisageant un Master en informatique comme solution alternative.
+
+        Les spécialisations qui m'intéressent le plus sont DevOps et Cybersécurité. Je souhaite donc postuler pour des écoles proposant ce type de formations, notamment à travers les candidatures effectuées au CESI, à Polytech Montpellier (formation DO), à 3IL Rodez, et à l’IMT Alès.
+
+        La faisabilité de ces formations est assurée grâce à la prolongation de mon alternance chez CA-TS pour une durée de 2 à 3 ans. Cela m’a été confirmé le 27/02/2025 par la personne chargée du recrutement de mon entreprise. Cela me garantit que l’alternance ne posera pas de problème.
+
+        Si toutefois mes candidatures dans ces écoles d’ingénieurs n'aboutissent pas, j’ai envisagé deux Masters qui ont une grande valeur ajoutée professionnelle et technique :
+
+        CNAM Montpellier, Master Cybersécurité : Cette formation spécialisée permettrait de consolider mes compétences en sécurité informatique tout en restant en alternance.
+
+        Epitech, MSc en alternance : Ayant déjà été admis dans cette formation, elle constitue une solution intégrable. Sa pédagogie axée sur les projets pratiques et l’alternance garantit une continuité professionnelle et technique.
+
+        En parallèle de l’acquisition de connaissances techniques, je souhaite également renforcer mes compétences humaines et mon savoir-être afin de devenir un professionnel complet. La collaboration, la communication efficace, la gestion du stress et la capacité à motiver une équipe sont autant d’aptitudes indispensables pour évoluer dans un environnement DevOps et sécuritaire. Je souhaite ainsi approfondir mon sens du leadership, mon adaptabilité, et ma faculté à résoudre les problèmes tout en restant à l’écoute des besoins de mon équipe. L’objectif est de devenir un acteur clé au sein d’une organisation, capable d’aligner les enjeux techniques et humains pour mener à bien des projets innovants.
+    `;
+
     return (
         <section className="py-12 px-6 text-center bg-gradient-to-b from-black via-gray-900 to-black text-white">
             <h2 className="text-4xl font-extrabold mb-6 text-yellow-500 drop-shadow-md">
@@ -86,7 +106,7 @@ export default function AboutMore() {
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-6xl mx-auto">
                 {/* Boîte 1 : Mon Parcours */}
                 <div
-                    className="relative bg-gradient-to-r from-gray-800 to-gray-700 text-white p-6 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300">
+                    className="relative bg-gradient-to-r from-gray-800 to-gray-700 text-white p-6 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300 justify-center">
                     <h3 className="text-2xl font-bold mb-4">Mon Parcours</h3>
                     <p className="line-clamp-3 text-gray-300">
                         Pour commencer, j'ai suivi mes études au lycée Jean Jaurès pour mon
@@ -102,7 +122,7 @@ export default function AboutMore() {
 
                 {/* Boîte 2 : Biographie */}
                 <div
-                    className="relative bg-gradient-to-r from-gray-700 to-gray-800 text-white p-6 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300">
+                    className="relative bg-gradient-to-r from-gray-700 to-gray-800 text-white p-6 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300 justify-center">
                     <h3 className="text-2xl font-bold mb-4">Biographie</h3>
                     <p className="line-clamp-3 text-gray-300">
                         Bonjour, je me nomme Léo Torres. Je suis né dans la magnifique région
@@ -110,6 +130,21 @@ export default function AboutMore() {
                     </p>
                     <button
                         onClick={() => toggleModal("biographie")}
+                        className="mt-6 w-full bg-yellow-500 text-black text-sm font-semibold py-3 px-4 rounded-md shadow-md hover:bg-yellow-600 hover:shadow-lg transition-all duration-300"
+                    >
+                        Lire la suite
+                    </button>
+                </div>
+
+                {/* Boîte 2 : Ma poursuite */}
+                <div
+                    className="relative bg-gradient-to-r from-gray-700 to-gray-800 text-white p-6 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300 justify-center">
+                    <h3 className="text-2xl font-bold mb-4">Ma poursuite</h3>
+                    <p className="line-clamp-3 text-gray-300">
+                        Actuellement étudiant en dernière année de BUT informatique parcours Déploiement d’Applications Communicantes et Sécurisées (DACS), j’ai effectué cette dernière année en alternance
+                    </p>
+                    <button
+                        onClick={() => toggleModal("Ma Poursuite")}
                         className="mt-6 w-full bg-yellow-500 text-black text-sm font-semibold py-3 px-4 rounded-md shadow-md hover:bg-yellow-600 hover:shadow-lg transition-all duration-300"
                     >
                         Lire la suite
@@ -166,19 +201,23 @@ export default function AboutMore() {
             {/* Modal */}
             {isModalOpen && (
                 <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
-                    <div className="bg-gray-800 p-8 rounded-lg max-w-3xl shadow-lg">
-                        <h2 className="text-3xl font-bold text-yellow-500 mb-4">
-                            {selectedModal === "parcours" ? "Mon Parcours" : "Biographie"}
-                        </h2>
-                        <p className="text-gray-300 text-lg leading-relaxed whitespace-pre-line">
-                            {selectedModal === "parcours" ? parcours : biography}
-                        </p>
-                        <button
-                            onClick={() => toggleModal(null)}
-                            className="mt-6 w-full bg-yellow-500 text-black text-sm font-semibold py-3 px-4 rounded-md shadow-md hover:bg-yellow-600 hover:shadow-lg transition-all duration-300"
-                        >
-                            Fermer
-                        </button>
+                    <div className="bg-gray-800 rounded-lg max-w-3xl max-h-[80vh] shadow-lg flex flex-col overflow-hidden">
+                        <div className="p-4 bg-gray-900">
+                            <h2 className="text-3xl font-bold text-yellow-500">
+                                {selectedModal === "parcours" ? "Mon Parcours" : selectedModal === "biographie" ? "Biographie" : "Ma Poursuite"}
+                            </h2>
+                        </div>
+                        <div className="text-gray-300 text-lg leading-relaxed whitespace-pre-line overflow-y-auto flex-grow p-8">
+                            {selectedModal === "parcours" ? parcours : selectedModal === "biographie" ? biography : poursuite}
+                        </div>
+                        <div className="p-4 bg-gray-900">
+                            <button
+                                onClick={() => toggleModal(null)}
+                                className="w-full bg-yellow-500 text-black text-sm font-semibold py-3 px-4 rounded-md shadow-md hover:bg-yellow-600 hover:shadow-lg transition-all duration-300"
+                            >
+                                Fermer
+                            </button>
+                        </div>
                     </div>
                 </div>
             )}
